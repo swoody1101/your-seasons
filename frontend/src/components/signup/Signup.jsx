fdbc95ce3dfc65495424e20a90eddbc9e03fec33
anveloper <hitedin@gmail.com>
2022-07-20 18:02:28 +0900

    feat: 회원가입 기능 추가, 컨설턴트 선택 항목만 추가하면 완료 컴포넌트 배치 완료됨

--- frontend/src/components/signup/Signup.jsx
+++ frontend/src/components/signup/Signup.jsx
@@ -2,37 +2,181 @@ import React, { useState } from 'react'
 import {
   Avatar,
   Button,
-  CssBaseline,
   TextField,
-  FormController,
+  Box,
+  CssBaseline,
   FormControlLabel,
   Checkbox,
-  FormHelperText,
   Grid,
-  Box,
   Typography,
-  Container
 } from '@mui/material'
+
+import ValidationInput from '../input/ValidationInput'
+import ComparePasswordInput from '../input/ConfirmPasswordInput'
+import ConfirmValidationInput from '../input/ConfirmValidationInput'
+import PhoneNumberInput from '../input/PhoneNumberInput'
+import BirthSelectInput from '../input/BirthSelectInput'
+import policy from './privacy_policy.txt'
+
+import regex from '../input/regex';
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 
-const signup = () => {
-  const theme = createTheme();
-  const [checked, setChcked] = useState(false);
+const SignUp = () => {
+  const [userEmail, setUserEmail] = useState('');
+  const [isEmailCheck, setIsEmailCheck] = useState(false);
+  const [password, setPassword] = useState('');
+  const [rePassword, setRePassword] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [isNicknameCheck, setIsNicknameCheck] = useState(false);
 
-  const handleAgree = (e) => {
-    setChcked(e.target.checked)
+  const [phoneNumber, setPhoneNumber] = useState('010');
+
+  const date = new Date();
+  let today = (date.getFullYear()) + '-' + ('0' + (date.getMonth() + 1)).slice(-2)
+    + '-' + ('0' + date.getDate()).slice(-2);
+  let beforeTw = (date.getFullYear() - 20) + '-' + ('0' + (date.getMonth() + 1)).slice(-2)
+    + '-' + ('0' + date.getDate()).slice(-2);
+  const [birth, setBirth] = useState(beforeTw);
+
+
+  const userInfo = {
+    email: userEmail,
+    password: password,
+    nickname: nickname,
+    phone: phoneNumber,
+    birth: birth,
   }
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: ''
+    }
+  }); // fontFamily 속성을 담을 위치만 넣어줘도 css에서 적용된 값으로 불러올 수 있다.
+
+  const [agreeChecked, setAgreeChcked] = useState(false);
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    console.log(e);
   }
 
-
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <Avatar sx={{
+        m: "1rem auto", bgcolor: 'secondary.main'
+      }} />
+      <CssBaseline />
+      <Grid container
+        direction="column"
+        justifyContent="center"
+        alignItems="center">
+        <Grid container item xs={12} sx={{}}>
+          <Grid item xs={12} sm={6} p={1} >
+            <ConfirmValidationInput
+              autofocus
+              label="이메일"
+              id="email"
+              value={userEmail}
+              setValue={setUserEmail}
+              isCheck={isEmailCheck}
+              setIsCheck={setIsEmailCheck}
+              handleValueCheck={() => {
+                alert("인증완료");
+                setIsEmailCheck(true);
+              }}
+              regexCheck={regex.email}
+              defaultText="이메일을 입력해주세요."
+              successText="success"
+              errorText="이메일 양식을 맞춰주세요."
+            />
 
-    </div>
+            <ValidationInput
+              label="패스워드"
+              type="password"
+              value={password}
+              setValue={setPassword}
+              regexCheck={regex.password}
+              maxValue={20}
+              defaultText="비밀번호를 입력해주세요."
+              successText="success"
+              errorText="소문자, 특수문자, 8~20글자 이상"
+            />
+
+            <ComparePasswordInput
+              label="패스워드 재확인"
+              type="password"
+              value={rePassword}
+              setValue={setRePassword}
+              compareValue={password}
+              maxValue={20}
+              defaultText="비밀번호를 다시한번 입력해주세요."
+              incorrectText="비밀번호가 일치하지 않습니다."
+              successText="success"
+              errorText="소문자, 특수문자, 8~20글자 이상"
+            />
+
+            <ConfirmValidationInput
+              autofocus
+              label="닉네임"
+              value={nickname}
+              setValue={setNickname}
+              isCheck={isNicknameCheck}
+              setIsCheck={setIsNicknameCheck}
+              handleValueCheck={() => {
+                alert("인증완료");
+                setIsNicknameCheck(true);
+              }}
+              regexCheck={regex.nickname}
+              defaultText="닉네임을 입력해주세요."
+              successText="success"
+              errorText="닉네임 양식을 맞춰주세요."
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} p={1} >
+            <PhoneNumberInput
+              label="전화번호"
+              type="text"
+              value={phoneNumber}
+              setValue={setPhoneNumber}
+              maxValue={13}
+              regexCheck={regex.phone}
+              defaultText="전화번호를 입력해주세요."
+              successText="success"
+              errorText="올바른 전화번호를 입력해주세요."
+            />
+
+            <BirthSelectInput
+              label="생년월일"
+              type="date"
+              value={birth}
+              setValue={setBirth}
+              maxValue={today}
+              defaultText="생일을 선택해주세요."
+              successText="success"
+              errorText="올바른 생년월일을 선택해주세요."
+            />
+
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+
+          <FormControlLabel
+            control={<Checkbox value={agreeChecked} onChange={e => { setAgreeChcked(e.target.value) }} color="primary" />}
+            label="회원가입 약관에 동의합니다."
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            onClick={() => { handleSubmit(userInfo) }}
+            sx={{ mt: 3, mb: 2, minWidth: 400, maxWidth: 600 }}
+            size="large"
+          >
+            회원가입
+          </Button>
+        </Grid>
+      </Grid>
+    </ThemeProvider >
   )
 }
 
-export default signup
+export default SignUp
