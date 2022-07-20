52daf53eb63018ecd2935005a7803b5bc4218e07
anveloper <hitedin@gmail.com>
2022-07-20 23:20:42 +0900

    chore: 라이브러리 추가 환경설정 변경

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -6,22 +6,25 @@ import {
 
 import './app.css'
 
+import NavBar from '../components/common/NavBar'
 import SignUp from '../components/signup/SignUp'
-import { Container } from '@mui/material'
-
+import { Box, Stack } from '@mui/material'
 
 const App = () => {
   return (
-    <Container>
-      <h3>어째서 글씨체가 적용되지 않는 건가</h3>
-      <Routes>
-        <Route path='/home' element={<p>home</p>} />
-        <Route path='/history' element={<p>history</p>} />
-        <Route path='/login' element={<p>login</p>} />
-        <Route path='/mypage' element={<p>mypage</p>} />
-        <Route path='/signup' element={<SignUp />} />
-      </Routes>
-    </Container>
+    <Box sx={{}}>
+      <NavBar />
+      <Stack direction="column" spacing={2} justifyContent="space-between">
+
+        <Routes>
+          <Route path='/home' element={<p>home</p>} />
+          <Route path='/history' element={<p>history</p>} />
+          <Route path='/login' element={<p>login</p>} />
+          <Route path='/mypage' element={<p>mypage</p>} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+      </Stack>
+    </Box>
   )
 }
 
