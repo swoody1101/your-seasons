8f8f4f5e1ba031b2c234861bfb0c86a8c66521d2
anveloper <hitedin@gmail.com>
2022-07-20 17:59:49 +0900

    feat: App 컴포넌트 수정

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -6,17 +6,22 @@ import {
 
 import './app.css'
 
+import SignUp from '../components/signup/SignUp'
+import { Container } from '@mui/material'
+
 
 const App = () => {
   return (
-    <div className='container app__container'>
+    <Container>
+      <h3>어째서 글씨체가 적용되지 않는 건가</h3>
       <Routes>
         <Route path='/home' element={<p>home</p>} />
         <Route path='/history' element={<p>history</p>} />
         <Route path='/login' element={<p>login</p>} />
-        <Route path='/signup' element={<p>signup</p>} />
+        <Route path='/mypage' element={<p>mypage</p>} />
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
-    </div>
+    </Container>
   )
 }
 
