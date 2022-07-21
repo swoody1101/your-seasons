197472235e8d8aafc5fb1b1372cb4544766aacb1
jeonyuneo <piq2255@gmail.com>
2022-07-22 02:06:18 +0900

    feat: Color 추가

---
+++ backend/src/main/java/com/yourseason/backend/common/domain/Color.java
@@ -0,0 +1,30 @@
+package com.yourseason.backend.common.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "color_id"))
+@Entity
+public class Color extends BaseTimeEntity {
+
+    @NotNull
+    private String hex;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tone_id")
+    private Tone tone;
+
+    @Builder
+    public Color(Long id, LocalDateTime createdTime, LocalDateTime lastModifiedTime, String hex, Tone tone) {
+        super(id, createdTime, lastModifiedTime);
+        this.hex = hex;
+        this.tone = tone;
+    }
+}
