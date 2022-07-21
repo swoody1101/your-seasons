59d44da4e13eea4b2d6aa3c408b8ab79f12aa5b6
jeonyuneo <piq2255@gmail.com>
2022-07-22 02:13:12 +0900

    feat: Review 추가

---
+++ backend/src/main/java/com/yourseason/backend/review/domain/Review.java
@@ -0,0 +1,38 @@
+package com.yourseason.backend.review.domain;
+
+import com.yourseason.backend.common.domain.BaseTimeEntity;
+import com.yourseason.backend.member.consultant.domain.Consultant;
+import com.yourseason.backend.member.customer.domain.Customer;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "review_id"))
+@Entity
+public class Review extends BaseTimeEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "consultant_id")
+    private Consultant consultant;
+
+    private double star;
+    private String comment;
+
+    @Builder
+    public Review(Long id, LocalDateTime createdTime, LocalDateTime lastModifiedTime,
+                  Customer customer, Consultant consultant, double star, String comment) {
+        super(id, createdTime, lastModifiedTime);
+        this.customer = customer;
+        this.consultant = consultant;
+        this.star = star;
+        this.comment = comment;
+    }
+}
