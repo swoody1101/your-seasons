cac57ab67f6591ba8c37a38b652f54880f58c260
jeonyuneo <piq2255@gmail.com>
2022-07-22 02:07:24 +0900

    feat: BestColorSet 추가

---
+++ backend/src/main/java/com/yourseason/backend/consulting/domain/result/BestColorSet.java
@@ -0,0 +1,26 @@
+package com.yourseason.backend.consulting.domain.result;
+
+import com.yourseason.backend.common.domain.BaseTimeEntity;
+import com.yourseason.backend.common.domain.ColorSet;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "best_color_set_id"))
+@Entity
+public class BestColorSet extends BaseTimeEntity {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "color_set_id")
+    private ColorSet colorSet;
+
+    @Builder
+    public BestColorSet(Long id, LocalDateTime createdTime, LocalDateTime lastModifiedTime, ColorSet colorSet) {
+        super(id, createdTime, lastModifiedTime);
+        this.colorSet = colorSet;
+    }
+}
