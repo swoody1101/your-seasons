559e0cab29cdffcd351e6ce3c4d79c6c653163c6
jeonyuneo <piq2255@gmail.com>
2022-07-22 00:18:21 +0900

    refactor: Member - validation 적용

--- backend/src/main/java/com/yourseason/backend/member/Member.java
+++ backend/src/main/java/com/yourseason/backend/member/Member.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,11 +15,27 @@ import java.time.LocalDate;
 @MappedSuperclass
 public abstract class Member extends BaseTimeEntity {
 
+    @NotNull
     private String email;
+
+    @NotNull
+    @Size(min = 6, max = 20)
     private String password;
+
+    @NotNull
+    @Size(min = 2, max = 20)
     private String name;
+
+    @NotNull
     private LocalDate birth;
+
+    @NotNull
+    @Size(min = 2, max = 10)
     private String nickname;
+
+    @NotNull
+    @Size(min = 12, max = 13)
     private String contact;
+
     private String imageUrl;
 }
