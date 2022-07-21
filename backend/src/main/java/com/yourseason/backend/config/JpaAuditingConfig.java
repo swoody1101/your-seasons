0a381ff58d701f6b5b75e2dd6586c9c072bc6195
jeonyuneo <piq2255@gmail.com>
2022-07-21 23:24:59 +0900

    chore: JpaAuditingConfig 추가

---
+++ backend/src/main/java/com/yourseason/backend/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.yourseason.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
