3017e25a2dfab12cff98f4b2152f0505c2eb3655
jeonyuneo <piq2255@gmail.com>
2022-07-22 02:13:58 +0900

    feat: Member - 필드 추가

--- backend/src/main/java/com/yourseason/backend/member/Member.java
+++ backend/src/main/java/com/yourseason/backend/member/Member.java
@@ -1,14 +1,21 @@
 package com.yourseason.backend.member;
 
 import com.yourseason.backend.common.domain.BaseTimeEntity;
+import com.yourseason.backend.consulting.domain.Consulting;
+import com.yourseason.backend.reservation.domain.Reservation;
+import com.yourseason.backend.review.domain.Review;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @MappedSuperclass
@@ -38,7 +45,16 @@ public abstract class Member extends BaseTimeEntity {
 
     private String imageUrl;
 
-    public Member(Long id, LocalDateTime createdTime, LocalDateTime lastModifiedTime, String email, String password, String name, LocalDate birth, String nickname, String contact, String imageUrl) {
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Consulting> consultings = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reservation> reservations = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+
+    public Member(Long id, LocalDateTime createdTime, LocalDateTime lastModifiedTime, String email, String password, String name, LocalDate birth, String nickname, String contact, String imageUrl, List<Consulting> consultings, List<Reservation> reservations, List<Review> reviews) {
         super(id, createdTime, lastModifiedTime);
         this.email = email;
         this.password = password;
@@ -47,5 +63,8 @@ public abstract class Member extends BaseTimeEntity {
         this.nickname = nickname;
         this.contact = contact;
         this.imageUrl = imageUrl;
+        this.consultings = consultings;
+        this.reservations = reservations;
+        this.reviews = reviews;
     }
 }
