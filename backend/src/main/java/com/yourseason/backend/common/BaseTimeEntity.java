a1e4cd75ff5a1886d24334c6740b5dd224bdd119
belle <ehoi.loveyourself@gmail.com>
2022-07-21 23:35:17 +0900

    feat: BaseTimeEntity 추가

---
+++ backend/src/main/java/com/yourseason/backend/common/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.yourseason.backend.common;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdTime;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedTime;
+}
