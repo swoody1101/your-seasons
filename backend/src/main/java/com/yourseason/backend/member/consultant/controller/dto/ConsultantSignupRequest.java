3057082419f19ee19bb97fb217ccf668b92a49e3
belle <ehoi.loveyourself@gmail.com>
2022-07-22 18:44:28 +0900

    feat: ConsultantSignupRequest 추가

---
+++ backend/src/main/java/com/yourseason/backend/member/consultant/controller/dto/ConsultantSignupRequest.java
@@ -0,0 +1,46 @@
+package com.yourseason.backend.member.consultant.controller.dto;
+
+import com.yourseason.backend.member.consultant.domain.Consultant;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ConsultantSignupRequest {
+
+    private String email;
+    private String password;
+    private String name;
+    private LocalDate birth;
+    private String nickname;
+    private String contact;
+    private Long licenseId;
+    private String licenseNumber;
+
+    @Builder
+    public ConsultantSignupRequest(String email, String password, String name, LocalDate birth,
+                                   String nickname, String contact, Long licenseId, String licenseNumber) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.birth = birth;
+        this.nickname = nickname;
+        this.contact = contact;
+        this.licenseId = licenseId;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public Consultant toEntity() {
+        return Consultant.builder()
+                .email(email)
+                .password(password)
+                .name(name)
+                .birth(birth)
+                .nickname(nickname)
+                .contact(contact)
+                .licenseNumber(licenseNumber)
+                .build();
+    }
+}
