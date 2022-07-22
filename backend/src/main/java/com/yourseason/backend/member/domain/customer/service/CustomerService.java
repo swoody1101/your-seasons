905ce4a147ac3648c92a0cfd2769beb5fa027be3
belle <ehoi.loveyourself@gmail.com>
2022-07-22 18:58:27 +0900

    refactor: 패키지 경로 변 및 @Transactional 어노테이션 추가

--- backend/src/main/java/com/yourseason/backend/member/domain/customer/service/CustomerService.java
+++ backend/src/main/java/com/yourseason/backend/member/domain/customer/service/CustomerService.java
@@ -1,18 +1,18 @@
-package com.yourseason.backend.member.customer.service;
+package com.yourseason.backend.member.domain.customer.service;
 
-import com.yourseason.backend.member.customer.controller.dto.CustomerSignupRequest;
-import com.yourseason.backend.member.customer.domain.CustomerRepository;
+import com.yourseason.backend.member.domain.customer.controller.dto.CustomerSignupRequest;
+import com.yourseason.backend.member.domain.customer.domain.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
 
-    @Transactional
     public void createCustomer(CustomerSignupRequest request) {
         customerRepository.save(request.toEntity());
     }
