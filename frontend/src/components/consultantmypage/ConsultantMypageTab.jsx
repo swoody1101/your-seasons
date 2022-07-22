a0631d256e182d31656dc3f0646d987fa0bb1a23
안성진 <hitedin@gmail.com>
2022-07-22 14:39:42 +0900

    fix: import 구문 수정

--- frontend/src/components/consultantmypage/ConsultantMypageTab.jsx
+++ frontend/src/components/consultantmypage/ConsultantMypageTab.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ConsultingResHistory from "./ConsultingResHistory";
 import ConsultingReview from "./ConsultingReview";
 
