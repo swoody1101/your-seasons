236b623a5e7852188556d7401e19ca99303db050
belle <ehoi.loveyourself@gmail.com>
2022-07-22 17:20:43 +0900

    feat: ConsultantRepository 추가

---
+++ backend/src/main/java/com/yourseason/backend/member/consultant/domain/ConsultantRepository.java
@@ -0,0 +1,6 @@
+package com.yourseason.backend.member.consultant.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultantRepository extends JpaRepository<Consultant, Long> {
+}
