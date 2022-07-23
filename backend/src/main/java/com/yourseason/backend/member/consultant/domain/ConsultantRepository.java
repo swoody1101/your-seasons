3b7407585fa6c7351cb87897ee18810daf1231fb
belle <ehoi.loveyourself@gmail.com>
2022-07-23 16:36:26 +0900

    style: 코드 컨벤션에 맞추어 메서드 사이에 빈 줄 삽입

--- backend/src/main/java/com/yourseason/backend/member/consultant/domain/ConsultantRepository.java
+++ backend/src/main/java/com/yourseason/backend/member/consultant/domain/ConsultantRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ConsultantRepository extends JpaRepository<Consultant, Long> {
 
     boolean existsByEmail(String email);
+
     boolean existsByNickname(String nickname);
 }
--- backend/src/main/java/com/yourseason/backend/member/customer/domain/CustomerRepository.java
+++ backend/src/main/java/com/yourseason/backend/member/customer/domain/CustomerRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     boolean existsByEmail(String email);
+
     boolean existsByNickname(String nickname);
 }
