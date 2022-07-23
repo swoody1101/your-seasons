8b76a121547c0a398e5aadc3f1abf778992cc28b
belle <ehoi.loveyourself@gmail.com>
2022-07-23 23:17:25 +0900

    refactor: DuplicateNicknameException 삭제

--- backend/src/main/java/com/yourseason/backend/member/exception/DuplicateNicknameException.java
+++
@@ -1,8 +0,0 @@
-package com.yourseason.backend.member.exception;
-
-public class DuplicateNicknameException extends RuntimeException {
-
-    public DuplicateNicknameException() {
-        super("닉네임이 중복됩니다.");
-    }
-}
