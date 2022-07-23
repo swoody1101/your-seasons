99ebf4c4f1451556d9e50f499140c4e6b6b8d91a
belle <ehoi.loveyourself@gmail.com>
2022-07-23 23:49:37 +0900

    refactor: 발생 에러 수정

--- backend/src/main/java/com/yourseason/backend/member/consultant/service/ConsultantService.java
+++ backend/src/main/java/com/yourseason/backend/member/consultant/service/ConsultantService.java
@@ -1,5 +1,6 @@
 package com.yourseason.backend.member.consultant.service;
 
+import com.yourseason.backend.common.exception.NotFoundException;
 import com.yourseason.backend.member.consultant.controller.dto.ConsultantSignupRequest;
 import com.yourseason.backend.member.consultant.domain.Consultant;
 import com.yourseason.backend.member.consultant.domain.ConsultantRepository;
@@ -12,13 +13,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class ConsultantService {
 
+    private static final String LICENSE_NOT_FOUND = "자격증이 존재하지 않습니다.";
+
     private ConsultantRepository consultantRepository;
     private LicenseRepository licenseRepository;
 
     public void createConsultant(ConsultantSignupRequest consultantSignupRequest) {
         Consultant consultant = consultantSignupRequest.toEntity();
-        License license = licenseRepository.findById(consultantSignupRequest.getLicenseId())
-                .orElseThrow(IllegalArgumentException::new);
+        License license = licenseRepository.findByName(consultantSignupRequest.getLicenseName())
+                .orElseThrow(() -> new NotFoundException(LICENSE_NOT_FOUND));
         consultant.registerLicense(license);
         consultantRepository.save(consultant);
     }
