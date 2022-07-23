27c5c43966f313c9bab193de3397fdab504c8d79
belle <ehoi.loveyourself@gmail.com>
2022-07-23 23:17:03 +0900

    refactor: DuplicateEmailException 삭제

--- backend/src/main/java/com/yourseason/backend/member/exception/DuplicateEmailException.java
+++
@@ -1,8 +0,0 @@
-package com.yourseason.backend.member.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-
-    public DuplicateEmailException() {
-        super("이메일이 중복됩니다.");
-    }
-}
