315fdfe8be4307b897cad3c7d264b20e6f771aff
belle <ehoi.loveyourself@gmail.com>
2022-07-23 23:53:24 +0900

    refactor: 사용하지 않는 임포트 정리

--- backend/src/main/java/com/yourseason/backend/member/controller/MemberController.java
+++ backend/src/main/java/com/yourseason/backend/member/controller/MemberController.java
@@ -1,11 +1,8 @@
 package com.yourseason.backend.member.controller;
 
-import com.yourseason.backend.common.exception.DuplicateEmailException;
-import com.yourseason.backend.common.exception.DuplicateNicknameException;
 import com.yourseason.backend.member.customer.controller.Message;
 import com.yourseason.backend.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
