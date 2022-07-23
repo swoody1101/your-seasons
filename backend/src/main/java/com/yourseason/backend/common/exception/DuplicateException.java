6aadd91fee58b9de4855fb124d9f0719e8809978
belle <ehoi.loveyourself@gmail.com>
2022-07-24 00:53:20 +0900

    refactor: DuplicateException -> DuplicationException 클래스명 수정

--- backend/src/main/java/com/yourseason/backend/common/exception/DuplicateException.java
+++
@@ -1,8 +0,0 @@
-package com.yourseason.backend.common.exception;
-
-public class DuplicateException extends RuntimeException{
-
-    public DuplicateException(String message) {
-        super(message);
-    }
-}
