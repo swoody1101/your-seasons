94912e2d69586f6faab3466f02f510176c7960a2
belle <ehoi.loveyourself@gmail.com>
2022-07-23 17:02:35 +0900

    style: 주석 삭제

--- backend/src/main/java/com/yourseason/backend/member/service/MemberService.java
+++ backend/src/main/java/com/yourseason/backend/member/service/MemberService.java
@@ -1,15 +1,11 @@
 package com.yourseason.backend.member.service;
 
-import com.yourseason.backend.member.domain.consultant.domain.ConsultantRepository;
-import com.yourseason.backend.member.domain.customer.domain.CustomerRepository;
-import com.yourseason.backend.member.exception.DuplicateEmailException;
-import com.yourseason.backend.member.exception.DuplicateNicknameException;
+import com.yourseason.backend.member.consultant.domain.ConsultantRepository;
+import com.yourseason.backend.member.customer.domain.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
-@Transactional
 @Service
 public class MemberService {
 
@@ -19,7 +15,6 @@ public class MemberService {
     public boolean validateEmail(String email) {
         if (customerRepository.existsByEmail(email) || consultantRepository.existsByEmail(email)) {
             return true;
-//            throw new DuplicateEmailException();
         }
         return false;
     }
@@ -27,7 +22,6 @@ public class MemberService {
     public boolean validateNickname(String nickname) {
         if (customerRepository.existsByNickname(nickname) || consultantRepository.existsByNickname(nickname)) {
             return true;
-//            throw new DuplicateNicknameException();
         }
         return false;
     }
