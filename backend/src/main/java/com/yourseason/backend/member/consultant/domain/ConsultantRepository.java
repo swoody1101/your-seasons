5fee8e2ab1bdd02382283323b40dba697edbf829
Swoody <swoody1101@gmail.com>
2022-07-24 00:38:17 +0900

    refactor: findByEmailAndPassword 메소드 수정

--- backend/src/main/java/com/yourseason/backend/member/consultant/domain/ConsultantRepository.java
+++ backend/src/main/java/com/yourseason/backend/member/consultant/domain/ConsultantRepository.java
@@ -1,9 +1,11 @@
 package com.yourseason.backend.member.consultant.domain;
 
-import com.yourseason.backend.member.controller.dto.LoginRequestDto;
-import com.yourseason.backend.member.controller.dto.LoginResponseDto;
+import com.yourseason.backend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ConsultantRepository extends JpaRepository<Consultant, Long> {
-    LoginResponseDto findByEmailAndPassword(LoginRequestDto loginRequestDto);
+
+    Member getByEmailAndPassword(String email, String password);
 }
