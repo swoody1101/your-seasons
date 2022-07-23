63376b1900e6c69fd51d11a0dbf897b3d6104710
Swoody <swoody1101@gmail.com>
2022-07-24 00:41:17 +0900

    refactor: getByEmailAndPassword 메서드 반환값과 매개변수 수정

--- backend/src/main/java/com/yourseason/backend/member/customer/domain/CustomerRepository.java
+++ backend/src/main/java/com/yourseason/backend/member/customer/domain/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.yourseason.backend.member.customer.domain;
 
-import com.yourseason.backend.member.controller.dto.LoginRequestDto;
-import com.yourseason.backend.member.controller.dto.LoginResponseDto;
+import com.yourseason.backend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    LoginResponseDto findByEmailAndPassword(LoginRequestDto loginRequestDto);
+
+    Member getByEmailAndPassword(String email, String password);
 }
