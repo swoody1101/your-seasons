32e7147f55a785908028246f0eccc59957ca46cc
jeonyuneo <piq2255@gmail.com>
2022-07-23 20:50:55 +0900

    feat: 컨설턴트 상세 조회(프로필 및 예약 내역) 기능 추가

--- backend/src/main/java/com/yourseason/backend/member/consultant/controller/ConsultantController.java
+++ backend/src/main/java/com/yourseason/backend/member/consultant/controller/ConsultantController.java
@@ -1,10 +1,12 @@
 package com.yourseason.backend.member.consultant.controller;
 
+import com.yourseason.backend.member.consultant.controller.dto.ConsultantListResponse;
 import com.yourseason.backend.member.consultant.controller.dto.ConsultantResponse;
 import com.yourseason.backend.member.consultant.service.ConsultantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,14 @@ public class ConsultantController {
     private final ConsultantService consultantService;
 
     @GetMapping
-    public ResponseEntity<List<ConsultantResponse>> getConsultants() {
-        return ResponseEntity.ok(consultantService.getConsultants());
+    public ResponseEntity<List<ConsultantListResponse>> getConsultants() {
+        return ResponseEntity.ok()
+                .body(consultantService.getConsultants());
+    }
+
+    @GetMapping("/{consultantId}")
+    public ResponseEntity<ConsultantResponse> getConsultant(@PathVariable Long consultantId) {
+        return ResponseEntity.ok()
+                .body(consultantService.getConsultant(consultantId));
     }
 }
--- backend/src/main/java/com/yourseason/backend/member/consultant/service/ConsultantService.java
+++ backend/src/main/java/com/yourseason/backend/member/consultant/service/ConsultantService.java
@@ -1,8 +1,12 @@
 package com.yourseason.backend.member.consultant.service;
 
+import com.yourseason.backend.common.exception.NotFoundException;
+import com.yourseason.backend.member.consultant.controller.dto.ConsultantListResponse;
 import com.yourseason.backend.member.consultant.controller.dto.ConsultantResponse;
+import com.yourseason.backend.member.consultant.controller.dto.ReservationListResponse;
 import com.yourseason.backend.member.consultant.domain.Consultant;
 import com.yourseason.backend.member.consultant.domain.ConsultantRepository;
+import com.yourseason.backend.reservation.domain.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,18 +17,48 @@ import java.util.stream.Collectors;
 @Service
 public class ConsultantService {
 
-    private final ConsultantRepository consultantRepository;
+    private static final String CONSULTANT_NOT_FOUND = "해당 컨설턴트를 찾을 수 없습니다.";
+    private static final String IMAGE_NOT_FOUND = "해당 이미지를 찾을 수 없습니다.";
 
-    public List<ConsultantResponse> getConsultants() {
+    private final ConsultantRepository consultantRepository;
+    private final ReservationRepository reservationRepository;
+
+    public List<ConsultantListResponse> getConsultants() {
         List<Consultant> consultants = consultantRepository.findAll();
         return consultants.stream()
                 .map(consultant ->
-                        ConsultantResponse.builder()
+                        ConsultantListResponse.builder()
                                 .consultantId(consultant.getId())
                                 .nickname(consultant.getNickname())
                                 .email(consultant.getEmail())
                                 .star(consultant.getStarAverage())
                                 .build())
                 .collect(Collectors.toList());
+    }
+
+    public ConsultantResponse getConsultant(Long consultantId) {
+        Consultant consultant = consultantRepository.findById(consultantId)
+                .orElseThrow(() -> new NotFoundException(CONSULTANT_NOT_FOUND));
+
+        List<ReservationListResponse> reservations = reservationRepository.findAllById(consultantId)
+                .stream()
+                .map(reservation -> ReservationListResponse.builder()
+                        .reservationId(reservation.getId())
+                        .reservationDate(reservation.getCreatedDate().toLocalDate())
+                        .reservationTime(reservation.getCreatedDate().toLocalTime())
+                        .request(reservation.getRequest())
+                        .build())
+                .collect(Collectors.toList());
+
+        return ConsultantResponse.builder()
+                .consultantId(consultantId)
+                .nickname(consultant.getNickname())
+                .contact(consultant.getContact())
+                .imageUrl(consultant.getImageUrl())
+                .introduction(consultant.getIntroduction())
+                .cost(consultant.getCost())
+                .licenseName(consultant.getLicense().getName())
+                .reservations(reservations)
+                .build();
     }
 }
