d29bb80fe409c39c426fc435fdab7112aeed067e
Swoody <swoody1101@gmail.com>
2022-07-24 00:51:57 +0900

    refactor: MemberService 클래스에서 인터페이스로 변환

--- backend/src/main/java/com/yourseason/backend/member/service/MemberService.java
+++ backend/src/main/java/com/yourseason/backend/member/service/MemberService.java
@@ -1,33 +1,9 @@
 package com.yourseason.backend.member.service;
 
-import com.yourseason.backend.member.consultant.domain.ConsultantRepository;
-import com.yourseason.backend.member.controller.dto.LoginRequestDto;
-import com.yourseason.backend.member.controller.dto.LoginResponseDto;
-import com.yourseason.backend.member.customer.domain.CustomerRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.yourseason.backend.member.controller.dto.LoginRequest;
+import com.yourseason.backend.member.controller.dto.LoginResponse;
 
-@RequiredArgsConstructor
-@Service
-public class MemberService {
+public interface MemberService {
 
-    private CustomerRepository customerRepository;
-    private ConsultantRepository consultantRepository;
-    private LoginResponseDto customerDto;
-    private LoginResponseDto consultantDto;
-
-    public LoginResponseDto getMemberInfo(LoginRequestDto loginRequestDto) {
-        if (loginRequestDto.getEmail() == null || loginRequestDto.getPassword() == null) {
-            return null;
-        } else {
-            customerDto = customerRepository.findByEmailAndPassword(loginRequestDto);
-            consultantDto = consultantRepository.findByEmailAndPassword(loginRequestDto);
-            if (customerDto != null) {
-                return customerDto;
-            } else if (consultantDto != null) {
-                return consultantDto;
-            }
-            return null;
-        }
-    }
+    LoginResponse login(LoginRequest loginRequest);
 }
