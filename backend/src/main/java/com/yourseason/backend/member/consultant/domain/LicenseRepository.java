924206c4626b58e7ee75e86919f062525d7dcba5
belle <ehoi.loveyourself@gmail.com>
2022-07-23 23:52:14 +0900

    feat: 이름으로 자격증 조회하는 메서드 추가

--- backend/src/main/java/com/yourseason/backend/member/consultant/domain/LicenseRepository.java
+++ backend/src/main/java/com/yourseason/backend/member/consultant/domain/LicenseRepository.java
@@ -1,6 +1,9 @@
-package com.yourseason.backend.member.domain.consultant.domain;
+package com.yourseason.backend.member.consultant.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LicenseRepository extends JpaRepository<License, Long> {
+    Optional<License> findByName(String licenseName);
 }
