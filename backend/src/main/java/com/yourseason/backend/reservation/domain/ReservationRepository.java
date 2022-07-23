b911d43674c2da697a86f1485de88a7c027f0019
jeonyuneo <piq2255@gmail.com>
2022-07-23 20:49:29 +0900

    feat: ReservationRepository 추가

---
+++ backend/src/main/java/com/yourseason/backend/reservation/domain/ReservationRepository.java
@@ -0,0 +1,10 @@
+package com.yourseason.backend.reservation.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    List<Reservation> findAllById(Long consultantId);
+}
