58cca8c46c571cf7957115625f5046429cd1affe
belle <ehoi.loveyourself@gmail.com>
2022-07-24 18:30:18 +0900

    feat: ReservationRepository 패키지 경로 수정

--- backend/src/main/java/com/yourseason/backend/reservation/domain/ReservationRepository.java
+++ backend/src/main/java/com/yourseason/backend/reservation/domain/ReservationRepository.java
@@ -1,6 +1,5 @@
-package com.yourseason.backend.review.domain;
+package com.yourseason.backend.reservation.domain;
 
-import com.yourseason.backend.reservation.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
