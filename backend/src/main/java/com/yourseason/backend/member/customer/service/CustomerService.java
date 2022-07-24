efa668c1cfa6b541c95e70078dc68c435f4aea4e
belle <ehoi.loveyourself@gmail.com>
2022-07-24 18:34:01 +0900

    refactor: 용어 수정

--- backend/src/main/java/com/yourseason/backend/member/customer/service/CustomerService.java
+++ backend/src/main/java/com/yourseason/backend/member/customer/service/CustomerService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerService {
 
-    private static final String USER_NOT_FOUND = "해당 사용자를 찾을 수 없습니다.";
+    private static final String CUSTOMER_NOT_FOUND = "해당 회원을 찾을 수 없습니다.";
 
     private final CustomerRepository customerRepository;
 
