d479e46aa325685c5b467b2b233d605f24b59e5a
belle <ehoi.loveyourself@gmail.com>
2022-07-25 00:50:21 +0900

    feat: ImageUploadException 추가

---
+++ backend/src/main/java/com/yourseason/backend/common/exception/ImageUploadException.java
@@ -0,0 +1,8 @@
+package com.yourseason.backend.common.exception;
+
+public class ImageUploadException extends RuntimeException {
+
+    public ImageUploadException(String message) {
+        super(message);
+    }
+}
