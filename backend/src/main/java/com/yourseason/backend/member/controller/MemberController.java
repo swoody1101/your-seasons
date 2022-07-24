f8509e401adbdcf9e9d47e0d8aa0074a1b9d5fcd
Swoody <swoody1101@gmail.com>
2022-07-24 14:50:24 +0900

    style: MemberService 추상화 제거 및 MemberServiceImpl을 MemberService로 변경

--- backend/src/main/java/com/yourseason/backend/member/controller/MemberController.java
+++ backend/src/main/java/com/yourseason/backend/member/controller/MemberController.java
@@ -2,7 +2,7 @@ package com.yourseason.backend.member.controller;
 
 import com.yourseason.backend.member.controller.dto.LoginRequest;
 import com.yourseason.backend.member.controller.dto.LoginResponse;
-import com.yourseason.backend.member.service.MemberServiceImpl;
+import com.yourseason.backend.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/members")
 public class MemberController {
 
-    private final MemberServiceImpl memberServiceImpl;
+    private final MemberService memberService;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         return ResponseEntity.ok()
-                .body(memberServiceImpl.login(loginRequest));
+                .body(memberService.login(loginRequest));
     }
 }
