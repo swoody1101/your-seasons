5200ea28e08d4f75ca29262c83c9b552c704817f
jeonyuneo <piq2255@gmail.com>
2022-07-25 00:25:05 +0900

    refactor: 미사용 import 삭제

--- backend/src/main/java/com/yourseason/backend/member/consultant/service/ConsultantService.java
+++ backend/src/main/java/com/yourseason/backend/member/consultant/service/ConsultantService.java
@@ -10,7 +10,6 @@ import com.yourseason.backend.member.consultant.domain.License;
 import com.yourseason.backend.member.consultant.domain.LicenseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
