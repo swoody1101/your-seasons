ddd3b1d5bb9c2265a6ae9b79cb807d207410899c
Swoody <swoody1101@gmail.com>
2022-07-25 00:43:01 +0900

    refactor: jwt 관련 함수 추가

--- backend/src/main/java/com/yourseason/backend/member/service/MemberService.java
+++ backend/src/main/java/com/yourseason/backend/member/service/MemberService.java
@@ -7,15 +7,20 @@ import com.yourseason.backend.member.controller.dto.LoginResponse;
 import com.yourseason.backend.member.customer.domain.CustomerRepository;
 import com.yourseason.backend.member.domain.Member;
 import com.yourseason.backend.member.domain.Role;
+import com.yourseason.backend.member.domain.SecurityMember;
 import com.yourseason.backend.member.exception.NotFoundException;
 import com.yourseason.backend.member.exception.WrongFormException;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private static final String NOT_FOUND_LOGIN_INFO = "이메일 혹은 패스워드가 입력되지 않았습니다.";
     private static final String NOT_FOUND_USER = "해당 사용자를 찾을 수 없습니다.";
@@ -33,12 +38,12 @@ public class MemberService {
         }
 
         Member loginMember;
-        Role role = Role.CUSTOMER;
-        Member customer = customerRepository.getByEmailAndPassword(email, password);
-        Member consultant = consultantRepository.getByEmailAndPassword(email, password);
+        Role role = Role.ROLE_CUSTOMER;
+        Member customer = customerRepository.findByEmailAndPassword(email, password);
+        Member consultant = consultantRepository.findByEmailAndPassword(email, password);
         if (customer == null && consultant != null) {
             loginMember = consultant;
-            role = Role.CONSULTANT;
+            role = Role.ROLE_CONSULTANT;
         } else if (customer != null && consultant == null) {
             loginMember = customer;
         } else {
@@ -50,6 +55,22 @@ public class MemberService {
                 .role(role)
                 .message("succeeded")
                 .build();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Member> findMember;
+        Optional<Member> customer = customerRepository.findByEmail(email);
+        Optional<Member> consultant = consultantRepository.findByEmail(email);
+
+        if (customer.isPresent() && !consultant.isPresent()) {
+            findMember = customer;
+        } else if (!customer.isPresent() && consultant.isPresent()) {
+            findMember = consultant;
+        } else {
+            throw new UsernameNotFoundException("가입되지 않는 email입니다.");
+        }
+        return new SecurityMember(findMember.get());
     }
 
     public void validateEmail(String email) {
