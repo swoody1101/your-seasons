552fe6e737a57e4c5f49cc00a219d0bb4606cd33
jeonyuneo <piq2255@gmail.com>
2022-07-24 22:53:03 +0900

    feat: ControllerAdvice 예외 처리 추가

--- backend/src/main/java/com/yourseason/backend/common/controller/ControllerAdvice.java
+++ backend/src/main/java/com/yourseason/backend/common/controller/ControllerAdvice.java
@@ -1,7 +1,10 @@
 package com.yourseason.backend.common.controller;
 
 import com.yourseason.backend.common.domain.Message;
+import com.yourseason.backend.common.exception.DuplicationException;
+import com.yourseason.backend.common.exception.ImageUploadException;
 import com.yourseason.backend.common.exception.NotFoundException;
+import com.yourseason.backend.common.exception.WrongFormException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,7 +18,28 @@ public class ControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
     public Message NotFoundException(RuntimeException runtimeException) {
-        log.debug(runtimeException.getMessage());
+        log.info(runtimeException.getMessage());
+        return new Message(runtimeException.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(DuplicationException.class)
+    public Message DuplicationException(RuntimeException runtimeException) {
+        log.info(runtimeException.getMessage());
+        return new Message(runtimeException.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WrongFormException.class)
+    public Message WrongFormException(RuntimeException runtimeException) {
+        log.info(runtimeException.getMessage());
+        return new Message(runtimeException.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(ImageUploadException.class)
+    public Message ImageUploadException(RuntimeException runtimeException) {
+        log.info(runtimeException.getMessage());
         return new Message(runtimeException.getMessage());
     }
 }
