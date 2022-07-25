9389abac46ab9bdb8f5246f7e775eb56dc58a919
belle <ehoi.loveyourself@gmail.com>
2022-07-25 23:48:45 +0900

    feat: WrongAccessException 추가

---
+++ backend/src/main/java/com/yourseason/backend/common/exception/WrongAccessException.java
@@ -0,0 +1,8 @@
+package com.yourseason.backend.common.exception;
+
+public class WrongAccessException extends CommonException {
+
+    public WrongAccessException(String message) {
+        super(message);
+    }
+}
