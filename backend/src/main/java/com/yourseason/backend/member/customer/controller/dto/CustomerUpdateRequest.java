93e3cc04db5c6bed9c665857a293cf6d930dbdbb
박태이 <ehoi.loveyourself@gmail.com>
2022-07-25 15:33:48 +0900

    fix: private 접근제한자 지정

--- backend/src/main/java/com/yourseason/backend/member/customer/controller/dto/CustomerUpdateRequest.java
+++ backend/src/main/java/com/yourseason/backend/member/customer/controller/dto/CustomerUpdateRequest.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CustomerUpdateRequest {
 
-    String nickname;
-    String contact;
+    private String nickname;
+    private String contact;
 }
