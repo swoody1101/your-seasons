38376d9461b766fd9e8f9dd52eb5675aafb0d101
Swoody <swoody1101@gmail.com>
2022-07-26 21:12:18 +0900

    feat: review 관련 dto 생성

---
+++ backend/src/main/java/com/yourseason/backend/review/controller/dto/ReviewCreateRequest.java
@@ -0,0 +1,20 @@
+package com.yourseason.backend.review.controller.dto;
+
+import com.yourseason.backend.review.domain.Review;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewCreateRequest {
+
+    private int star;
+    private String comment;
+
+    public Review toEntity() {
+        return Review.builder()
+                .star(star)
+                .comment(comment)
+                .build();
+    }
+}
