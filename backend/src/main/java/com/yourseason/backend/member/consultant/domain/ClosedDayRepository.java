5e6f93a50c465ef20e04d89190a308807cd6c97e
Swoody <swoody1101@gmail.com>
2022-07-28 01:58:38 +0900

    feat: ClosedDayRepository.java 추가

---
+++ backend/src/main/java/com/yourseason/backend/member/consultant/domain/ClosedDayRepository.java
@@ -0,0 +1,5 @@
+package com.yourseason.backend.member.consultant.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClosedDayRepository extends JpaRepository<ClosedDay, Long> {}
