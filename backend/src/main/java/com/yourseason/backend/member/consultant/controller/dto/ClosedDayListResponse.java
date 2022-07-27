1fc5da5f1a6b8541bfa4603d0b339035a1498f89
Swoody <swoody1101@gmail.com>
2022-07-28 00:15:42 +0900

    refactor: 자료형 변경

--- backend/src/main/java/com/yourseason/backend/member/consultant/controller/dto/ClosedDayListResponse.java
+++ backend/src/main/java/com/yourseason/backend/member/consultant/controller/dto/ClosedDayListResponse.java
@@ -12,6 +12,6 @@ import java.time.LocalDate;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ClosedDayListResponse {
 
-    private long closedDayId;
+    private Long closedDayId;
     private LocalDate date;
 }
