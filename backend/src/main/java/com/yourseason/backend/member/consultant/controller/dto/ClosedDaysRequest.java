b16308fb2d61896da377506bf9ceb31e3d1a8dd3
Swoody <swoody1101@gmail.com>
2022-07-28 01:58:58 +0900

    feat: ClosedDaysRequest.java 추가

---
+++ backend/src/main/java/com/yourseason/backend/member/consultant/controller/dto/ClosedDaysRequest.java
@@ -0,0 +1,26 @@
+package com.yourseason.backend.member.consultant.controller.dto;
+
+import com.yourseason.backend.member.consultant.domain.ClosedDay;
+import com.yourseason.backend.member.consultant.domain.ClosedDayRepository;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ClosedDaysRequest {
+
+    private List<LocalDate> closedDays;
+
+    public List<ClosedDay> toEntityList() {
+        return closedDays.stream()
+                .map(closedDay -> ClosedDay.builder()
+                        .date(closedDay)
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
