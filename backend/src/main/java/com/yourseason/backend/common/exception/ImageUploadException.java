5acd461dd56406c02d36a59d05275baf4704bdde
어정윤 <piq2255@naver.com>
2022-07-28 14:43:01 +0900

    refactor: 미사용 클래스 삭제

--- backend/src/main/java/com/yourseason/backend/common/exception/ImageUploadException.java
+++
@@ -1,8 +0,0 @@
-package com.yourseason.backend.common.exception;
-
-public class ImageUploadException extends CommonException {
-
-    public ImageUploadException(String message) {
-        super(message);
-    }
-}
