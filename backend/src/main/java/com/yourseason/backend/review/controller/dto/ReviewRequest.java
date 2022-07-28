8e917f7a76301f9ccc7cbd6d4cf2a63511adbbc5
swoody <swoody1101@gmail.com>
2022-07-29 01:07:05 +0900

    refactor: AllArgsConstructor 어노테이션 추가

--- backend/src/main/java/com/yourseason/backend/review/controller/dto/ReviewRequest.java
+++ backend/src/main/java/com/yourseason/backend/review/controller/dto/ReviewRequest.java
@@ -2,10 +2,12 @@ package com.yourseason.backend.review.controller.dto;
 
 import com.yourseason.backend.review.domain.Review;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReviewRequest {
 
