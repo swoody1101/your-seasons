025e7bd85f6fd071455a7036505136326e3f599c
박태이 <ehoi.loveyourself@gmail.com>
2022-07-28 09:01:14 +0900

    feat: 기본생성자의 접근 제한 막기

--- backend/src/main/java/com/yourseason/backend/member/consultant/controller/dto/ConsultantInfoResponse.java
+++ backend/src/main/java/com/yourseason/backend/member/consultant/controller/dto/ConsultantInfoResponse.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Getter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ConsultantInfoResponse {
 
     String name;
