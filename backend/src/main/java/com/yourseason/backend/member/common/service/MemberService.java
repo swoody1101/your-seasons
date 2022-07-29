be5c51cf872b231e25c637b5ebe6d809ed99fccc
jeonyuneo <piq2255@gmail.com>
2022-07-29 15:15:23 +0900

    fix: 이메일 불일치 예외 처리 추가

--- backend/src/main/java/com/yourseason/backend/member/common/service/MemberService.java
+++ backend/src/main/java/com/yourseason/backend/member/common/service/MemberService.java
@@ -67,11 +67,11 @@ public class MemberService {
         Member loginMember;
         Member customer = customerRepository.findByEmail(email);
         Member consultant = consultantRepository.getByEmail(email);
-        if (consultant == null) {
+        if (customer != null && consultant == null) {
             checkValidPassword(password, customer.getPassword());
             loginMember = customer;
             member.put("role", String.valueOf(Role.CUSTOMER));
-        } else if (customer == null) {
+        } else if (consultant != null && customer == null) {
             checkValidPassword(password, consultant.getPassword());
             loginMember = consultant;
             member.put("role", String.valueOf(Role.CONSULTANT));
