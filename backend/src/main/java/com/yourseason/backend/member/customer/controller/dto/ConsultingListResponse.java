b1d23c41c9aabb08a1a78d7e6efec35c805de696
belle <ehoi.loveyourself@gmail.com>
2022-07-31 19:35:43 +0900

    fix: bestColorSet 객체 자체가 아닌 hex값만 리턴하도록 수정

--- backend/src/main/java/com/yourseason/backend/member/customer/controller/dto/ConsultingListResponse.java
+++ backend/src/main/java/com/yourseason/backend/member/customer/controller/dto/ConsultingListResponse.java
@@ -5,6 +5,7 @@ import com.yourseason.backend.consulting.domain.result.WorstColorSet;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Builder
@@ -18,8 +19,8 @@ public class ConsultingListResponse {
     private String consultantImageUrl;
     private LocalDate consultingDate;
     private String tone;
-    private BestColorSet bestColorSet;
-    private WorstColorSet worstColorSet;
+    private List<ColorSetResponse> bestColorSet;
+    private List<ColorSetResponse> worstColorSet;
     private String resultImageUrl;
     private String comment;
     private boolean hasReview;
