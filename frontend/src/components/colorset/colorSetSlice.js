a42188b0fe716d2be6d961011eb4430fb6f3ebc4
dabeen <elqla19@gmail.com>
2022-07-31 13:20:17 +0900

    fix: 소문자파일명 삭제

--- frontend/src/components/colorset/colorSetSlice.js
+++ frontend/src/components/colorset/colorSetSlice.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, current } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import Axios from '../../api/Axios'
 import {OK} from '../../api/CustomConst'
 const colorset = [
