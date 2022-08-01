5eb8094a52ee2169cf5b12841ebfa81760354794
anveloper <hitedin@gmail.com>
2022-08-01 14:52:11 +0900

    refactor: 인증 헤더값 요청 변경

--- frontend/src/api/Axios.js
+++ frontend/src/api/Axios.js
@@ -25,6 +25,7 @@ export default Axios;
 Axios.interceptors.request.use((config) => {
   if (getToken()) {
     config.headers.Authorization = `Bearer ${getToken()}`;
+    // config.headers["X-Auth-Token"] = `Bearer ${getToken()}`;
   }
   return config;
 });
