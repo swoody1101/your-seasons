90e4a54f3bd9bfa091715ff09f328183c8aede3c
어정윤 <piq2255@naver.com>
2022-08-01 17:34:38 +0900

    feat: 컬러 관련 엔터티들 getter 추가

--- backend/src/main/java/com/yourseason/backend/common/domain/Color.java
+++ backend/src/main/java/com/yourseason/backend/common/domain/Color.java
@@ -2,12 +2,14 @@ package com.yourseason.backend.common.domain;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AttributeOverride(name = "id", column = @Column(name = "color_id"))
 @Entity
