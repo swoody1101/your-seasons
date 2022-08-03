5f640f0566e8c9b49066f25a105280a7848533ad
Swoody <swoody1101@gmail.com>
2022-08-03 11:48:33 +0900

    feat: 로그인 응답 수정

--- backend/src/main/java/com/yourseason/backend/member/common/controller/dto/LoginResponse.java
+++ backend/src/main/java/com/yourseason/backend/member/common/controller/dto/LoginResponse.java
@@ -6,20 +6,28 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LoginResponse {
 
+    private String name;
     private String nickname;
+    private LocalDate birth;
+    private String contact;
+    private String email;
     private String imageUrl;
     private Role role;
-    private String message;
 
     @Builder
-    public LoginResponse(String nickname, String imageUrl, Role role, String message) {
+    public LoginResponse(String name, String nickname, LocalDate birth, String contact, String email, String imageUrl, Role role) {
+        this.name = name;
         this.nickname = nickname;
+        this.birth = birth;
+        this.contact = contact;
+        this.email = email;
         this.imageUrl = imageUrl;
         this.role = role;
-        this.message = message;
     }
 }
