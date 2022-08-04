44b98080056b90818b0bb4057ea39cd983eb382a
swoody <swoody1101@gmail.com>
2022-08-04 16:50:52 +0900

    feat: NotRegisterException 추가

---
+++ backend/src/main/java/com/yourseason/backend/common/exception/NotRegisterException.java
@@ -0,0 +1,8 @@
+package com.yourseason.backend.common.exception;
+
+public class NotRegisterException extends CommonException{
+
+    public NotRegisterException(String message) {
+        super(message);
+    }
+}
