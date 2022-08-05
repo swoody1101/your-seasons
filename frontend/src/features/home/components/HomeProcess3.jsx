419ee01eaf7a1ed0c3ce857c46f02b60afe68cff
dabeen <elqla19@gmail.com>
2022-08-05 14:08:58 +0900

    design: 홈 컴포넌트 재배치 및 디자인 2차 완료 추후 처음뜨는 계절 캐로셀 사진 문구와 맞게   변경 예정

--- frontend/src/features/home/components/HomeProcess3.jsx
+++ frontend/src/features/home/components/HomeProcess3.jsx
@@ -4,13 +4,16 @@ import ExampleImg from '../../../assets/images/homeSlide/ExampleDiagnosis.jpg'
 
 const HomeProcess3 = () => {
 	return (<BigGrid container>
-		<Grid1 item xs={3}>
-			<Text>
-				다양한 컬러셋과 함께, 컨설턴트 진단표 제공
-			</Text>
+		<Grid1 item xs={6}>
+			<MainTypography>
+				컬러셋과 함께, 컨설턴트 진단표 제공
+			</MainTypography>
+			<SubTypography>
+				다양한 컨설턴트의 진단을 받아보세요
+			</SubTypography>
 		</Grid1>
 		
-		<Grid2 item xs={9}>
+		<Grid2 item xs={6}>
 			<ColorsetImg src = {ExampleImg} />
 		</Grid2>
 	</BigGrid>
@@ -19,56 +22,49 @@ const HomeProcess3 = () => {
 
 export default HomeProcess3
 
-const BigGrid = styled(Grid)((
-	{
-		position: 'relative',
-		backgroundColor: '#aab8bb',
-		height: '100vh',
-		width: '100vw',
-	}
-))
+const BigGrid = styled(Grid)({
+	position: 'relative',
+	backgroundColor: '#aab8bb',
+	height: '100vh',
+	width: '100vw',
+})
 
 
-const Grid1 = styled(Grid)((
-	{
+const Grid1 = styled(Grid)({
+	position: 'relative',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	flexDirection: 'column',
+})
 
-	}
-))
+const Grid2 = styled(Grid)({
+	position: 'relative',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	flexDirection: 'column',
+})
 
-const Grid2 = styled(Grid)((
-	{
-		filter: 'blur(7px)',
-	}
-))
+const MainTypography = styled(Typography)({
+	position: 'absolute',
+	fontFamily: 'malgunbd !important',
+	fontSize: 'var(--font-title-size)',
+	letterSpacing: 'var(--font-letter-spacing)',
+	color:  '#000000', //"#b4004f",
+	paddingBottom: 15,
+})
 
-const ColorsetImg = styled('img')((
-	{
-		width: '10%'
-	}
-))
+const SubTypography = styled(Typography)({
+	position: 'absolute',
+	marginTop: 70,
+	fontFamily: 'malgunbd !important',
+	fontSize: 'var(--font-sub-size)',
+	letterSpacing: 'var(--font-letter-spacing)',
+  color: '#00000080',
+})
 
-
-const Text = styled(Typography)((
-  {
-    fontFamily: 'malgun !important',
-    position: "relative",
-		fontSize: '2.5rem',
-		letterSpacing: -3,
-    top: '15vh',
-    left: '15vh',
-    color:  '#FFFFFF', //"#b4004f",
-		// textShadow: '1px 2px 2px pink',
-    "@keyframes textIn": {
-      from: {
-        transform: "translateX(-10rem)",
-				opacity: 0,
-      },
-      to: {
-        transform: "translateX(0)",
-				opacity: 1,
-      }
-    },
-    animation: "textIn 1s ease",
-    animationDirection: "alternate"
-  }
-))
+const ColorsetImg = styled('img')({
+	filter: 'blur(5px)',
+	width: '20vw',
+})
