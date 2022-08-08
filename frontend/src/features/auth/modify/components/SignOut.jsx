e46546c12933348ad75da2220f749d8bd69da3d9
Sang Jin <tkdwls8412@naver.com>
2022-08-08 21:11:59 +0900

    feat: 로그아웃 모달 생성 및 회원탈퇴 후 로그아웃/홈 이동 구현

--- frontend/src/features/auth/modify/components/SignOut.jsx
+++ frontend/src/features/auth/modify/components/SignOut.jsx
@@ -1,38 +1,9 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux/es/exports';
-import { useNavigate } from 'react-router-dom';
-
-import { Box, Typography, Button, ButtonGroup } from '@mui/material'
-
-
-import { BAD_REQUEST, NOT_FOUND, CONFLICT } from 'api/CustomConst'
-import { signOut } from 'features/auth/authSlice'
+import { Box, Typography, ButtonGroup } from '@mui/material'
+import SignOutModal from './SignOutModal';
 
 const SignOut = () => {
-  const { role } = useSelector((state) => state.auth.logonUser)
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const cancel = () => {
-    navigate('/mypage')
-  }
-  const handleSignOut = () => {
-    dispatch(signOut(role))
-      .unwrap()
-      .then((res) => {
-        alert("수정이 완료되었습니다.")
-      })
-      .catch((err) => {
-        if (err.status === BAD_REQUEST) {
-          alert('적절한 요청이 아닙니다.')
-        } else if (err.status === NOT_FOUND) {
-          alert('없는 아이디 이거나 잘못된 정보입니다.')
-        } else if (err.status === CONFLICT) {
-          alert('거절된 내용입니다.')
-        }
-      })
-  }
   return (
     <Box
       fullWidth
@@ -49,12 +20,7 @@ const SignOut = () => {
           justifyContent: "end",
         }}
       >
-        <Button
-          onClick={cancel} variant="contained"
-        >취소</Button>
-        <Button
-          onClick={handleSignOut} variant="contained" color="error"
-        >확인</Button>
+        <SignOutModal></SignOutModal>
       </ButtonGroup>
     </Box >
   )
