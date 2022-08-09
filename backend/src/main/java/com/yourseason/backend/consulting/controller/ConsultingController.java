e0166bd83a4f418d36345ce6c00cd933fa13ed10
jeonyuneo <piq2255@gmail.com>
2022-08-09 21:06:44 +0900

    feat: ConsultingController 컨설팅 입장 기능 추가

--- backend/src/main/java/com/yourseason/backend/consulting/controller/ConsultingController.java
+++ backend/src/main/java/com/yourseason/backend/consulting/controller/ConsultingController.java
@@ -1,14 +1,13 @@
 package com.yourseason.backend.consulting.controller;
 
 import com.yourseason.backend.consulting.controller.dto.ConsultingCreateResponse;
+import com.yourseason.backend.consulting.controller.dto.ConsultingJoinRequest;
+import com.yourseason.backend.consulting.controller.dto.ConsultingJoinResponse;
 import com.yourseason.backend.consulting.service.ConsultingService;
 import com.yourseason.backend.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +20,11 @@ public class ConsultingController {
     public ResponseEntity<ConsultingCreateResponse> createConsulting(@RequestHeader("Authorization") String token) {
         return ResponseEntity.ok()
                 .body(consultingService.createConsulting(JwtUtil.getMemberId(token)));
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<ConsultingJoinResponse> joinConsulting(@RequestHeader("Authorization") String token, @RequestBody ConsultingJoinRequest consultingJoinRequest) {
+        return ResponseEntity.ok()
+                .body(consultingService.joinConsulting(JwtUtil.getMemberId(token), consultingJoinRequest));
     }
 }
