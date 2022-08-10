0abd9461038780b2a0775d90d2e166040bb3dc6e
swoody <swoody1101@gmail.com>
2022-08-10 13:33:01 +0900

    chore: Redis 포트 지정

---
+++ backend/src/main/java/com/yourseason/backend/util/RedisUtil.java
@@ -0,0 +1,34 @@
+package com.yourseason.backend.util;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Service
+public class RedisUtil {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public String getData(String key) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public void setDataExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public boolean validateData(String key, String value) {
+        if (getData(key).equals(value)) {
+            return true;
+        }
+        return false;
+    }
+}
