e4fbd93fde5f6c30bc59d65c3bb7bdaf30c42c13
anveloper <hitedin@gmail.com>
2022-08-10 13:51:36 +0900

    feat: 예약기능과 연동된 컨설팅 룸 입장 로직 구현

--- frontend/src/common/consulting/ConsultButton.jsx
+++ frontend/src/common/consulting/ConsultButton.jsx
@@ -3,67 +3,47 @@ import { useDispatch, useSelector } from 'react-redux/es/exports';
 import { useNavigate, Link } from 'react-router-dom';
 
 import { CUSTOMER, CONSULTANT, BAD_REQUEST, NOT_FOUND, CONFLICT } from 'api/CustomConst'
-import { getConsultantSessionName } from 'features/consulting/consultingRoom/consultSlice'
+import { myResFetch, consultingResFetch } from 'features/mypage/mypageSlice'
 
+import JoinResList from './JoinResList'
 import { Box, Button, Typography, styled, Input, ButtonGroup } from '@mui/material'
 
 
 const ConsultButton = () => {
   const { role } = useSelector(state => state.auth.logonUser)
-  const { consultantSessionName } = useSelector(state => state.consult.consultantSessionName)
 
   const [isInput, setIsInput] = useState(false)
-  const [consultantNickname, setConsultantNickname] = useState('')
 
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   const handlePop = () => {
     setIsInput(!isInput)
+    if (role === CONSULTANT) {
+      dispatch(consultingResFetch())
+        .then(() => { })
+    }
+    if (role === CUSTOMER) {
+      dispatch(myResFetch())
+        .then(() => { })
+    }
   }
 
-  const handleJoin = () => {
-    dispatch(getConsultantSessionName(consultantNickname))
-      .then((res) => {
-        console.log(res)
-        navigate('/consult')
-      })
-      .catch((err) => {
-        if (err.response.status === BAD_REQUEST) {
-          alert('적절한 요청이 아닙니다.')
-        } else if (err.response.status === NOT_FOUND) {
-          alert('개설된 세션이 없습니다.')
-        } else if (err.response.status === CONFLICT) {
-          alert('이미 중복으로 접속된 세션입니다.')
-        }
-      })
-    setIsInput(false)
-    setConsultantNickname('')
-  }
+
   return (
     <Box>
       {
-        isInput &&
+        role && isInput &&
         <SBox gap={1}>
-          <Typography display='inline'>
-            컨설턴트 닉네임
-          </Typography>
-          <SInput
-            value={consultantNickname}
-            onChange={(e) => {
-              setConsultantNickname(e.target.value)
-            }}
-          />
+          <JoinResList />
           <ButtonGroup>
             <Button onClick={handlePop} variant="contained">닫기</Button>
-            <Button onClick={handleJoin} variant="contained">입장</Button>
           </ButtonGroup>
         </SBox>
       }
       {
         role === CONSULTANT &&
-        <SButton variant="contained">
-          <Link to="/consult">방만들기</Link>
+        <SButton onClick={handlePop} variant="contained">
+          <Link to="#">방만들기</Link>
         </SButton>
       }
       {
@@ -84,18 +64,13 @@ const SBox = styled(Box)({
   border: '1rem',
   borderRadius: '1rem',
   width: '15rem',
-  aspectRatio: '3/1',
   position: 'fixed',
   right: '3rem',
   bottom: '3rem',
   zIndex: '20001',
   display: 'flex',
-  flexDirection: 'column'
-})
-
-const SInput = styled(Input)({
-
-  backgroundColor: 'white',
+  flexDirection: 'column',
+  justifyContent: 'space-between'
 })
 
 const SButton = styled(Button)({
