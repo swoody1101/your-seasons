88e8a28cf21b0e53320f8eb43157dd43fb1951aa
Sang Jin <tkdwls8412@naver.com>
2022-08-12 13:54:03 +0900

    feat: 코멘트/진단결과톤/진단결과이미지 업로드 컴포넌트 구성

--- frontend/src/common/colorset/MainColorSet.jsx
+++ frontend/src/common/colorset/MainColorSet.jsx
@@ -1,22 +1,32 @@
 import React, { useState } from 'react'
-import { useSelector } from 'react-redux';
-import { Box, Grid, styled, Tab } from '@mui/material'
+import { useSelector, useDispatch } from 'react-redux';
+import { Box, Grid, styled, Tab, TextField, MenuItem, Button } from '@mui/material'
 
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 import ColorPaletteBox from './PaletteBox'
 import Drawing from 'common/drawing/Drawing';
-import { VerticalAlignCenter } from '@mui/icons-material';
+import { changeComment, selectTone, setFiles } from './colorSetSlice';
 
 
-const MainColorSet = ({ setIsBest, setIsWorst }) => {
-	const selectedColor = useSelector(state => state.colorSetList.selectedColor)
-	console.log(selectedColor)
+
+const MainColorSet = () => {
+	const dispatch = useDispatch()
+	const comment = useSelector(state => state.colorSetList.comment)
+	const selectedTone = useSelector(state => state.colorSetList.tone)
+
 	const [
 		{ spring_bright, spring_true, spring_light },
 		{ summer_light, summer_true, summer_soft },
 		{ autumn_soft, autumn_true, autumn_dark },
 		{ winter_bright, winter_true, winter_dark }
 	] = useSelector(state => state.colorSetList.data)
+	const tone = [
+		'',
+		'봄 브라이트', '봄 트루', '봄 라이트',
+		'여름 라이트', '여름 트루', '여름 소프트',
+		'가을 소프트', '가을 트루', '가을 다크',
+		'겨울 브라이트', '겨울 트루', '겨울 다크',
+	]
 	const [value, setValue] = useState(1);
 	const handleChange = (event, newValue) => {
 		setValue(newValue);
@@ -27,15 +37,24 @@ const MainColorSet = ({ setIsBest, setIsWorst }) => {
 	const handleSubChange = (event, newValue) => {
 		setSubValue(newValue);
 	};
+
+	const [isDrawing, setIsDrwaing] = useState(true)
+
+	const onLodaFile = (event) => {
+		const file = event.target.files
+		dispatch(setFiles(file))
+	}
+
 	return (
 		<SGrid item xs={12}>
 			<TabContext value={value} sx={{ height: '100%' }}>
 				<Box sx={{ borderBottom: 'divider' }}>
 					<TabList onChange={handleChange} sx={{ borderBottom: 'divider' }}>
-						<Tab label="봄" value={1} />
-						<Tab label="여름" value={2} />
-						<Tab label="가을" value={3} />
-						<Tab label="겨울" value={4} />
+						<Tab label="봄" value={1} onClick={() => { setIsDrwaing(true) }} />
+						<Tab label="여름" value={2} onClick={() => { setIsDrwaing(true) }} />
+						<Tab label="가을" value={3} onClick={() => { setIsDrwaing(true) }} />
+						<Tab label="겨울" value={4} onClick={() => { setIsDrwaing(true) }} />
+						<Tab label="진단결과" value={5} onClick={() => { setIsDrwaing(false) }} />
 					</TabList>
 				</Box>
 				<CTabPanel value={1}>
@@ -130,8 +149,47 @@ const MainColorSet = ({ setIsBest, setIsWorst }) => {
 						</Box>
 					</TabContext>
 				</CTabPanel>
+				<CTabPanel value={5}>
+					<TabContext value={subValue} sx={{ height: '100%', width: '100%' }}>
+						진단결과
+						<TextField
+							size='small'
+							label="코멘트"
+							name="request"
+							margin="normal"
+							value={comment}
+							onChange={(e) => { dispatch(changeComment(e.target.value)) }}
+							multiline
+							rows={1}
+							autoComplete="request"
+							autoFocus
+							required
+							fullWidth
+							helperText="진단 결과에 대한 컨설턴트님의 의견을 남겨주십시오" />
+						<TextField
+							id="tone-select"
+							select
+							label='톤 결과'
+							size='small'
+							value={selectedTone}
+							onChange={(e) => { dispatch(selectTone(e.target.value)) }}
+							helperText="내담자의 톤 결과를 골라주세요">
+							{tone.map((tone, idx) => (
+								<MenuItem key={idx} value={tone}>
+									{tone}
+								</MenuItem>
+							))}
+						</TextField>
+						<div>
+							<Button variant="contained" component="label">
+								진단표 업로드하기
+								<input hidden accept="image/*" multiple type="file" onChange={onLodaFile} />
+							</Button>
+						</div>
+					</TabContext>
+				</CTabPanel>
 			</TabContext>
-			<Drawing />
+			{isDrawing ? <Drawing /> : null}
 		</SGrid>
 
 	)
@@ -154,5 +212,4 @@ const CTab = styled(Tab)({
 
 const CTabPanel = styled(TabPanel)({
 	padding: "0px",
-	marginBottom: "12px",
 })
