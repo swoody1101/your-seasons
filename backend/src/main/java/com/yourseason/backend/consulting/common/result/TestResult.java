87768f69d17db2817b28e186be70d8d85833764f
belle <ehoi.loveyourself@gmail.com>
2022-08-15 19:05:09 +0900

    feat: TestResult 추상 클래스 생성

---
+++ backend/src/main/java/com/yourseason/backend/consulting/common/result/TestResult.java
@@ -0,0 +1,38 @@
+package com.yourseason.backend.consulting.common.result;
+
+import com.yourseason.backend.common.domain.BaseTimeEntity;
+import com.yourseason.backend.common.domain.Tone;
+import com.yourseason.backend.consulting.common.BestColorSet;
+import com.yourseason.backend.consulting.common.WorstColorSet;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
+public abstract class TestResult extends BaseTimeEntity {
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "best_color_set_id")
+    private BestColorSet bestColorSet;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "worst_color_set_id")
+    private WorstColorSet worstColorSet;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tone_id")
+    private Tone tone;
+
+    public TestResult(Long id, LocalDateTime createdDate, LocalDateTime lastModifiedDate, LocalDateTime deletedDate,
+                      BestColorSet bestColorSet, WorstColorSet worstColorSet, Tone tone) {
+        super(id, createdDate, lastModifiedDate, deletedDate, true);
+        this.bestColorSet = bestColorSet;
+        this.worstColorSet = worstColorSet;
+        this.tone = tone;
+    }
+}
