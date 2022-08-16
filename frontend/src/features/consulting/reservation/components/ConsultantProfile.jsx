323f3030d69cfda7abeba7d190305793ab20ed35
Sang Jin <tkdwls8412@naver.com>
2022-08-16 15:11:21 +0900

    design: 컨설팅 예약페이지 디자인 수정

--- frontend/src/features/consulting/reservation/components/ConsultantProfile.jsx
+++ frontend/src/features/consulting/reservation/components/ConsultantProfile.jsx
@@ -7,64 +7,65 @@ import MyAvatar from "common/avatar/MyAvatar";
 import { ConsultantDetailFetch, ConsultingReviewFetch } from "features/consulting/consultantListSlice";
 
 const ConsultantProfile = () => {
-  const dispatch = useDispatch()
-  const consultantId = useParams().id
-  const { nickname, introduction, cost, starAverage, imageUrl } = useSelector(state => state.consultantList.consultantDetail)
-  useEffect(() => {
-    dispatch(ConsultantDetailFetch(consultantId))
-  }, [dispatch])
+	const dispatch = useDispatch()
+	const consultantId = useParams().id
+	const { nickname, introduction, cost, starAverage, imageUrl } = useSelector(state => state.consultantList.consultantDetail)
+	useEffect(() => {
+		dispatch(ConsultantDetailFetch(consultantId))
+	}, [dispatch])
 
-  useEffect(() => {
-    dispatch(ConsultingReviewFetch(consultantId))
-  }, [dispatch])
+	useEffect(() => {
+		dispatch(ConsultingReviewFetch(consultantId))
+	}, [dispatch])
 
-  return (
-			<Grid container >			
-				{/* 이미지 */}
-				<Grid item xs={12} sm={3} sx={{
-					display: 'flex',
-					justifyContent: 'center',
-				}}>
-					<MyAvatar setSize={16} imgUrl={imageUrl}/>
-				</Grid>
-				{/* 프로필 텍스트 */}
-				<Grid item xs={12} sm={9}>
-					<ProfileText>
+	return (
+		<Grid container sx={{ backgroundColor: "white", borderRadius: "10px" }} >
+			{/* 이미지 */}
+			<Grid item xs={12} sm={3} sx={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}>
+				<MyAvatar setSize={16} imgUrl={imageUrl} />
+			</Grid>
+			{/* 프로필 텍스트 */}
+			<Grid item xs={12} sm={9}>
+				<ProfileText>
 					<MainText>
 						<h3>{nickname} 컨설턴트님</h3>
-						<div>{starAverage===0? '등록된 별점이 없습니다.' :  <StarRating starrating={starAverage} />}</div>
-						</MainText>
-						<Introduction>{introduction ? introduction : '등록된 자기소개가 없습니다.'}</Introduction>
-						<p>진단비용 {cost ? cost + '원' : '등록된 비용이 없습니다.'}</p>
-					</ProfileText>
-				</Grid>
+						<div>{starAverage === 0 ? '등록된 별점이 없습니다.' : <StarRating starrating={starAverage} />}</div>
+					</MainText>
+					<Introduction>{introduction ? introduction : '등록된 자기소개가 없습니다.'}</Introduction>
+					<p>진단비용 {cost ? cost + '원' : '등록된 비용이 없습니다.'}</p>
+				</ProfileText>
 			</Grid>
-  )
+		</Grid>
+	)
 }
 
 export default ConsultantProfile
 
 
 const ProfileText = styled(Stack)({
-  display: "flex",
-  flexDirection: "column",
-  paddinTop: "20px",
-  paddingLeft: "20px",
+	display: "flex",
+	flexDirection: "column",
+	paddinTop: "20px",
+	paddingLeft: "20px",
 	height: "180px",
 	justifyContent: "space-evenly",
 })
 
 const Introduction = styled('div')({
-  display: "-webkit-box",
+	display: "-webkit-box",
 	WebkitBoxOrient: "vertical",
 	WebkitLineClamp: "3",
 	maxWidth: 600,
-  overflow: "hidden",
+	overflow: "hidden",
 	lineHeight: "100%",
 })
 
 
 const MainText = styled('div')({
-  display: "flex",
+	display: "flex",
 	gap: "5%",
 })
