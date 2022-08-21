e7346a7cc1ffe7144b2f57ebf8690187868ed4cf
swoody <swoody1101@gmail.com>
2022-08-21 20:17:35 +0900

    refactor: 자가진단 개설 함수 자가진단 종료 함수로 통폐합

--- backend/src/main/java/com/yourseason/backend/consulting/self/service/SelfConsultingService.java
+++ backend/src/main/java/com/yourseason/backend/consulting/self/service/SelfConsultingService.java
@@ -1,12 +1,9 @@
 package com.yourseason.backend.consulting.self.service;
 
 import com.yourseason.backend.common.domain.*;
-import com.yourseason.backend.common.exception.InternalServerErrorException;
 import com.yourseason.backend.common.exception.NotFoundException;
-import com.yourseason.backend.common.exception.WrongAccessException;
 import com.yourseason.backend.consulting.common.domain.BestColorSet;
 import com.yourseason.backend.consulting.common.domain.WorstColorSet;
-import com.yourseason.backend.consulting.self.controller.dto.SelfConsultingCreateResponse;
 import com.yourseason.backend.consulting.self.controller.dto.SelfConsultingFinishRequest;
 import com.yourseason.backend.consulting.self.domain.SelfConsulting;
 import com.yourseason.backend.consulting.self.domain.SelfConsultingRepository;
@@ -26,9 +23,6 @@ public class SelfConsultingService {
 
     private static final String COLOR_NOT_FOUND = "해당 색상을 찾을 수 없습니다.";
     private static final String CUSTOMER_NOT_FOUND = "해당 회원을 찾을 수 없습니다.";
-    private static final String FAIL_TO_SAVE_SELF_CONSULTING_INFO = "자가진단 정보 저장에 실패했습니다.";
-    private static final String SELF_CONSULTING_NOT_FOUND = "해당 자가진단을 찾을 수 없습니다.";
-    private static final String WRONG_ACCESS = "잘못된 접근입니다.";
     private static final String EMAIL_FORMAT = "[@.]";
     private static final String SESSION_DELIMITER = "-";
     private static final int BEST_TONE = 0;
@@ -38,25 +32,14 @@ public class SelfConsultingService {
     private final ToneRepository toneRepository;
     private final SelfConsultingRepository selfConsultingRepository;
 
-    public SelfConsultingCreateResponse createSelfConsulting(Long customerId) {
-        Customer customer = customerRepository.findById(customerId)
-                .orElseThrow(() -> new NotFoundException(CUSTOMER_NOT_FOUND));
-        customer.createSelfConsulting(getNewSelfConsulting(customer, getSessionId(customer)));
-        customerRepository.save(customer);
-        return SelfConsultingCreateResponse.builder()
-                .selfConsultingId(getSelfConsultingId(customer))
-                .build();
-    }
-
     @Transactional
     public Message finishSelfConsulting(Long customerId, SelfConsultingFinishRequest selfConsultingFinishRequest) {
         Customer customer = customerRepository.findById(customerId)
                 .orElseThrow(() -> new NotFoundException(CUSTOMER_NOT_FOUND));
-        SelfConsulting selfConsulting = selfConsultingRepository.findById(selfConsultingFinishRequest.getSelfConsultingId())
-                .orElseThrow(() -> new NotFoundException(SELF_CONSULTING_NOT_FOUND));
-        if (!customer.equals(selfConsulting.getCustomer())) {
-            throw new WrongAccessException(WRONG_ACCESS);
-        }
+
+        SelfConsulting selfConsulting = getNewSelfConsulting(customer, getSessionId(customer));
+        customer.createSelfConsulting(selfConsulting);
+        customerRepository.save(customer);
 
         ColorSet requestBestColorSet = ColorSet.builder().build();
         toColorColorSet(requestBestColorSet, selfConsultingFinishRequest.getBestColorSet());
@@ -109,15 +92,6 @@ public class SelfConsultingService {
                 .customer(customer)
                 .sessionId(sessionId)
                 .build();
-    }
-
-    private Long getSelfConsultingId(Customer customer) {
-        return customer.getSelfConsultings()
-                .stream()
-                .filter(SelfConsulting::isActive)
-                .map(SelfConsulting::getId)
-                .findFirst()
-                .orElseThrow(() -> new InternalServerErrorException(FAIL_TO_SAVE_SELF_CONSULTING_INFO));
     }
 
     private void toColorColorSet(ColorSet requestColorSet, List<String> colorSet) {
